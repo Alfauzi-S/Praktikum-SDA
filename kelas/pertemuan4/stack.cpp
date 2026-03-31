613f2a0040b9a224fb29776441cbe386186df326
Alfauzi-S <muhammad.alfauzi256@gmail.com>
2026-03-31 15:43:37 +0800

    pertemuan 4

---
+++ kelas/pertemuan4/stack.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#define MAX 100
+using namespace std;
+int stack[MAX];
+int top = -1; // Menandakan elemen teratas pada stack
+
+void push(int value)
+{
+    if (top >= MAX - 1)
+    {
+        cout << "Stack overflow" << endl;
+    }
+    else
+    {
+        top++;
+        stack[top] = value;
+        cout << value << " pushed into stack" << endl;
+    }
+}
+
+int pop()
+{
+    if (top < 0)
+    {
+        cout << "Stack underflow" << endl;
+        return -1;
+    }
+    else
+    {
+        int value = stack[top];
+        top--;
+        return value;
+    }
+}
+int peek()
+{
+    if (top < 0)
+    {
+        cout << "Stack is empty" << endl;
+        return -1;
+    }
+    else
+    {
+        return stack[top];
+    }
+}
+
+bool isEmpty()
+{
+    return (top < 0);
+}
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    cout << peek();
+    pop();
+    cout << peek();
+}
